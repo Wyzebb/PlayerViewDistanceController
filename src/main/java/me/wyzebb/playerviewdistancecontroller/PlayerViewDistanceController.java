3cfc4e4e570cb6fa85186e816b012d94542af86c
Wyzebb <BenSJSmith8@gmail.com>
2024-07-22 23:39:27 +0100

    Add support for prefixes.yml file

--- src/main/java/me/wyzebb/playerviewdistancecontroller/PlayerViewDistanceController.java
+++ src/main/java/me/wyzebb/playerviewdistancecontroller/PlayerViewDistanceController.java
@@ -3,17 +3,26 @@ package me.wyzebb.playerviewdistancecontroller;
 import me.wyzebb.playerviewdistancecontroller.commands.GlobalViewDistanceCommand;
 import me.wyzebb.playerviewdistancecontroller.commands.ViewDistanceCommand;
 import me.wyzebb.playerviewdistancecontroller.events.JoinLeaveEvent;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.IOException;
+
 public final class PlayerViewDistanceController extends JavaPlugin {
+    private File prefixesConfigFile;
+    private FileConfiguration prefixesConfig;
 
     @Override
     public void onEnable() {
         getLogger().info("Plugin started!");
 
         // Config
-        getConfig().options().copyDefaults(true);
         saveDefaultConfig();
+        getConfig().options().copyDefaults(true);
+        createPrefixesConfig();
 
         // Register join and leave events
         getServer().getPluginManager().registerEvents(new JoinLeaveEvent(this), this);
@@ -21,6 +30,20 @@ public final class PlayerViewDistanceController extends JavaPlugin {
         // Register Commands
         getCommand("viewdistance").setExecutor(new ViewDistanceCommand(this));
         getCommand("globalviewdistance").setExecutor(new GlobalViewDistanceCommand(this));
+    }
+
+    public FileConfiguration getPrefixesConfig() {
+        return this.prefixesConfig;
+    }
+
+    private void createPrefixesConfig() {
+        prefixesConfigFile = new File(getDataFolder(), "prefixes.yml");
+        if (!prefixesConfigFile.exists()) {
+            prefixesConfigFile.getParentFile().mkdirs();
+            saveResource("prefixes.yml", false);
+        }
+
+        prefixesConfig = YamlConfiguration.loadConfiguration(prefixesConfigFile);
     }
 
     @Override
