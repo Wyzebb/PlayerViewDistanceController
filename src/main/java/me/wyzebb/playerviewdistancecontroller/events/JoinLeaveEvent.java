f41a882ad5757fe866dc709a6e55de991ef445a0
Wyzebb <BenSJSmith8@gmail.com>
2024-07-23 22:41:50 +0100

    Remove debugging code, improve variables, handle exceptions

--- src/main/java/me/wyzebb/playerviewdistancecontroller/events/JoinLeaveEvent.java
+++ src/main/java/me/wyzebb/playerviewdistancecontroller/events/JoinLeaveEvent.java
@@ -2,6 +2,7 @@ package me.wyzebb.playerviewdistancecontroller.events;
 
 import me.wyzebb.playerviewdistancecontroller.PlayerViewDistanceController;
 import me.wyzebb.playerviewdistancecontroller.data.PlayerDataHandler;
+import me.wyzebb.playerviewdistancecontroller.utility.ClampAmountUtility;
 import me.wyzebb.playerviewdistancecontroller.utility.PlayerUtility;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -36,33 +37,28 @@ public class JoinLeaveEvent implements Listener {
         if (playerDataFile.exists()) {
             FileConfiguration cfg = YamlConfiguration.loadConfiguration(playerDataFile);
             amount = cfg.getInt("chunks");
-            plugin.getLogger().info("1");
 
             if (amount == plugin.getConfig().getInt("default-distance")) {
                 // Default so redirect to prefixes
-                int pre = checkPrefixes(amount, e, playerDataFile, dataHandler);
-                if (!(pre == 1000)) {
-                    amount = pre;
+                int errorCheck = checkPrefixes(amount, e, playerDataFile, dataHandler);
+                if (!(errorCheck == 1000)) {
+                    amount = errorCheck;
                 }
             }
         } else {
-            int pre = checkPrefixes(amount, e, playerDataFile, dataHandler);
-            if (!(pre == 1000)) {
-                amount = pre;
+            int errorCheck = checkPrefixes(amount, e, playerDataFile, dataHandler);
+            if (!(errorCheck == 1000)) {
+                amount = errorCheck;
             }
 
         }
 
-        amount = Math.min(32, amount);
-        amount = Math.max(2, amount);
+        amount = ClampAmountUtility.clampChunkValue(amount, plugin);
 
-        amount = Math.min(plugin.getConfig().getInt("max-distance"), amount);
-        amount = Math.max(plugin.getConfig().getInt("min-distance"), amount);
-
-        if (!((playerDataFile.exists())) && !(plugin.getConfig().getBoolean("set-default-distance"))) {
-            amount = Math.min(32, plugin.getConfig().getInt("max-distance"));
-            amount = Math.max(2, plugin.getConfig().getInt("min-distance"));
-        }
+//        if (!((playerDataFile.exists())) && !(plugin.getConfig().getBoolean("set-default-distance"))) {
+//            amount = Math.min(32, plugin.getConfig().getInt("max-distance"));
+//            amount = Math.max(2, plugin.getConfig().getInt("min-distance"));
+//        }
 
         dataHandler.setChunks(amount);
         e.getPlayer().setViewDistance(amount);
@@ -86,60 +82,45 @@ public class JoinLeaveEvent implements Listener {
     }
 
 
-    private int checkPrefixes(Integer chunkAmount, PlayerJoinEvent event, File playerDataFile, PlayerDataHandler dataHandler) {
+    private int checkPrefixes(Integer amount, PlayerJoinEvent event, File playerDataFile, PlayerDataHandler dataHandler) {
         Set<String> keys = plugin.getPrefixesConfig().getConfigurationSection("prefixes").getKeys(false);
-        plugin.getLogger().info("2");
         if (!keys.isEmpty()) {
-            plugin.getLogger().info("Keys: " + keys.toString());
-            plugin.getLogger().info("3");
+            plugin.getLogger().info("Loaded prefixes: " + keys.toString()); // UNNECESSARY: FOR DEBUGGING
 
+            // UNNECESSARY: FOR DEBUGGING
             for (String key : keys) {
-                plugin.getLogger().info("4");
-                plugin.getLogger().info("Key: " + key);
-                plugin.getLogger().info("Value: " + plugin.getPrefixesConfig().getInt(("prefixes." + key)));
-
-
+                plugin.getLogger().info("Prefix: " + key);
+                plugin.getLogger().info("Distance: " + plugin.getPrefixesConfig().getInt(("prefixes." + key)));
             }
         } else {
-            plugin.getLogger().info("5");
             plugin.getLogger().info("No keys found or keys set is null");
-            return 1000;
+            return 1000; // ERROR
         }
-        plugin.getLogger().info("6");
+
         for (String key : keys) {
-            plugin.getLogger().info("7");
             if (Objects.equals(key, "dot")) {
                 key = ".";
             }
-            plugin.getLogger().info("8");
             if (event.getPlayer().getName().toLowerCase().startsWith(key.toLowerCase())) {
                 // Name starts with prefix
 
-                chunkAmount = plugin.getPrefixesConfig().getInt(("prefixes." + key));
+                amount = plugin.getPrefixesConfig().getInt(("prefixes." + key));
+                amount = ClampAmountUtility.clampChunkValue(amount, plugin);
 
+//                if (!((playerDataFile.exists())) && !(plugin.getConfig().getBoolean("set-default-distance"))) {
+//                    amount = Math.min(32, plugin.getConfig().getInt("max-distance"));
+//                    amount = Math.max(2, plugin.getConfig().getInt("min-distance"));
+//                }
 
-                chunkAmount = Math.min(32, chunkAmount);
-                chunkAmount = Math.max(2, chunkAmount);
-
-                chunkAmount = Math.min(plugin.getConfig().getInt("max-distance"), chunkAmount);
-                chunkAmount = Math.max(plugin.getConfig().getInt("min-distance"), chunkAmount);
-
-                if (!((playerDataFile.exists())) && !(plugin.getConfig().getBoolean("set-default-distance"))) {
-                    chunkAmount = Math.min(32, plugin.getConfig().getInt("max-distance"));
-                    chunkAmount = Math.max(2, plugin.getConfig().getInt("min-distance"));
-                }
-
-                dataHandler.setChunks(chunkAmount);
-                event.getPlayer().setViewDistance(chunkAmount);
+                dataHandler.setChunks(amount);
+                event.getPlayer().setViewDistance(amount);
 
                 String msg = "SET YOUR VIEW DISTANCE TO {chunks} chunks because of your name's prefix";
-                msg = msg.replace("{chunks}", String.valueOf(chunkAmount));
+                msg = msg.replace("{chunks}", String.valueOf(amount));
                 event.getPlayer().sendMessage(msg);
-
-
             }
         }
-        return chunkAmount;
+        return amount;
     }
 
 
@@ -153,7 +134,7 @@ public class JoinLeaveEvent implements Listener {
 
         try {
             cfg.save(playerDataFile);
-        } catch (IOException event) {
+        } catch (Exception event) {
             plugin.getLogger().warning("An error occurred saving the player view distance data!");
         }
 
