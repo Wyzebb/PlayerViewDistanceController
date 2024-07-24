f9fbd255b2d9e14c29f1b92782cd5f2615937365
Wyzebb <BenSJSmith8@gmail.com>
2024-07-24 15:27:25 +0100

    Fix bug with sender detection

--- src/main/java/me/wyzebb/playerviewdistancecontroller/commands/CommandManager.java
+++ src/main/java/me/wyzebb/playerviewdistancecontroller/commands/CommandManager.java
@@ -4,6 +4,7 @@ import me.wyzebb.playerviewdistancecontroller.PlayerViewDistanceController;
 import me.wyzebb.playerviewdistancecontroller.commands.subcommands.SetCommand;
 import me.wyzebb.playerviewdistancecontroller.commands.subcommands.SetOnlineCommand;
 import me.wyzebb.playerviewdistancecontroller.commands.subcommands.SubCommand;
+import me.wyzebb.playerviewdistancecontroller.utility.SendHelpMsgUtility;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -15,42 +16,29 @@ import java.util.ArrayList;
 public class CommandManager implements CommandExecutor {
 
     private final ArrayList<SubCommand> subcommands = new ArrayList<>();
+    private final PlayerViewDistanceController plugin;
 
     public CommandManager(PlayerViewDistanceController plugin){
+        this.plugin = plugin;
         subcommands.add(new SetCommand(plugin));
         subcommands.add(new SetOnlineCommand(plugin));
     }
 
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command cmd, @NotNull String s, @NotNull String[] args) {
-
-        if (commandSender instanceof Player p){
-            if (args.length > 0){
-                for (int i = 0; i < getSubcommands().size(); i++){
-                    if (args[0].equalsIgnoreCase(getSubcommands().get(i).getName())) {
-                        getSubcommands().get(i).performCommand(p, args);
-                    }
+        if (args.length > 0){
+            for (int i = 0; i < getSubcommands().size(); i++){
+                if (args[0].equalsIgnoreCase(getSubcommands().get(i).getName())) {
+                    getSubcommands().get(i).performCommand(commandSender, args);
                 }
-            } else {
-                sendSubcommands(p);
             }
-
+        } else {
+            SendHelpMsgUtility.sendHelpMessage(commandSender, plugin);
         }
-
-
         return true;
-    }
-
-    private void sendSubcommands(Player p) {
-        p.sendMessage("--------------------------------");
-        for (int i = 0; i < getSubcommands().size(); i++){
-            p.sendMessage(getSubcommands().get(i).getSyntax() + " - " + getSubcommands().get(i).getDescription());
-        }
-        p.sendMessage("--------------------------------");
     }
 
     public ArrayList<SubCommand> getSubcommands(){
         return subcommands;
     }
-
 }
