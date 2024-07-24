d464276b315585a6c4da2ba0e709164772229a26
Wyzebb <BenSJSmith8@gmail.com>
2024-07-24 14:39:33 +0100

    Added encapsulation for max and min possible values

--- src/main/java/me/wyzebb/playerviewdistancecontroller/utility/ClampAmountUtility.java
+++ src/main/java/me/wyzebb/playerviewdistancecontroller/utility/ClampAmountUtility.java
@@ -4,9 +4,16 @@ import me.wyzebb.playerviewdistancecontroller.PlayerViewDistanceController;
 
 public class ClampAmountUtility {
 
+    private static final int MAX_POSSIBLE = 32;
+    private static final int MIN_POSSIBLE = 2;
+
+    public static int getMaxPossible() {
+        return MAX_POSSIBLE;
+    }
+
     public static int clampChunkValue(int amount, PlayerViewDistanceController plugin) {
-        amount = Math.min(32, amount);
-        amount = Math.max(2, amount);
+        amount = Math.min(MAX_POSSIBLE, amount);
+        amount = Math.max(MIN_POSSIBLE, amount);
 
         amount = Math.min(plugin.getConfig().getInt("max-distance"), amount);
         amount = Math.max(plugin.getConfig().getInt("min-distance"), amount);
