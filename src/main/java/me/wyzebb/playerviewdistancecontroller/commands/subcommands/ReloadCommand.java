1e0856b54b01eb627005995aaa49b71828e19751
Wyzebb <BenSJSmith8@gmail.com>
2024-08-09 13:27:11 +0100

    Add new ReloadCommand subcommand

---
+++ src/main/java/me/wyzebb/playerviewdistancecontroller/commands/subcommands/ReloadCommand.java
@@ -0,0 +1,60 @@
+package me.wyzebb.playerviewdistancecontroller.commands.subcommands;
+
+import me.wyzebb.playerviewdistancecontroller.PlayerViewDistanceController;
+import me.wyzebb.playerviewdistancecontroller.utility.ProcessConfigMessagesUtility;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+
+public class ReloadCommand extends SubCommand {
+
+    private FileConfiguration prefixesConfig;
+
+    private final PlayerViewDistanceController plugin;
+
+    public ReloadCommand(PlayerViewDistanceController plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public String getName() {
+        return "reload";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Reload the plugin's config files";
+    }
+
+    @Override
+    public String getSyntax() {
+        return "/vd reload";
+    }
+
+    @Override
+    public void performCommand(CommandSender commandSender, String[] args) {
+        plugin.saveDefaultConfig();
+        plugin.getConfig().options().copyDefaults(true);
+        createPrefixesConfig();
+        ProcessConfigMessagesUtility.processMessage("reload-config-msg", commandSender);
+    }
+
+    public FileConfiguration getPrefixesConfig() {
+        return this.prefixesConfig;
+    }
+
+    private void createPrefixesConfig() {
+        File prefixesConfigFile = new File(plugin.getDataFolder(), "prefixes.yml");
+        if (!prefixesConfigFile.exists()) {
+            boolean folder = prefixesConfigFile.getParentFile().mkdirs();
+            if (!folder) {
+                plugin.getLogger().warning("An error occured when reloading config");
+            }
+            plugin.saveResource("prefixes.yml", false);
+        }
+
+        prefixesConfig = YamlConfiguration.loadConfiguration(prefixesConfigFile);
+    }
+}
