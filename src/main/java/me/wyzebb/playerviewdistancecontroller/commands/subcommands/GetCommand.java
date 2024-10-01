4ae42152f5acc9facef4fb24d848a818ddad1425
Wyzebb <BenSJSmith8@gmail.com>
2024-10-01 17:59:31 +0100

    Add get subcommand for getting a player's current max view distance

---
+++ src/main/java/me/wyzebb/playerviewdistancecontroller/commands/subcommands/GetCommand.java
@@ -0,0 +1,66 @@
+package me.wyzebb.playerviewdistancecontroller.commands.subcommands;
+
+import me.wyzebb.playerviewdistancecontroller.utility.PlayerUtility;
+import me.wyzebb.playerviewdistancecontroller.utility.ProcessConfigMessagesUtility;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class GetCommand extends SubCommand {
+
+    @Override
+    public String getName() {
+        return "get";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Displays the targeted player's max view distance";
+    }
+
+    @Override
+    public String getSyntax() {
+        return "/vd get [player]";
+    }
+
+    @Override
+    public void performCommand(CommandSender commandSender, String[] args) {
+        if (args.length < 1 || args.length > 2) {
+            ProcessConfigMessagesUtility.processMessage("incorrect-args", commandSender);
+        } else {
+            if (args.length == 1) {
+                if (commandSender instanceof Player player) {
+                    if (commandSender.hasPermission("pvdc.get-self")) {
+                        ProcessConfigMessagesUtility.processMessage("self-view-distance-get-msg", commandSender, PlayerUtility.getPlayerDataHandler(player).getChunks());
+                    } else {
+                        ProcessConfigMessagesUtility.processMessage("no-permission", commandSender);
+                    }
+                } else {
+                    ProcessConfigMessagesUtility.processMessage("incorrect-args", commandSender);
+                }
+
+            } else {
+                String targetName = args[1];
+                Player target = Bukkit.getServer().getPlayerExact(targetName);
+
+                if (target == null) {
+                    ProcessConfigMessagesUtility.processMessage("player-offline-msg", commandSender);
+
+                } else if (commandSender == target) {
+                    if (commandSender.hasPermission("pvdc.get-self")) {
+                        ProcessConfigMessagesUtility.processMessage("self-view-distance-get-msg", commandSender, PlayerUtility.getPlayerDataHandler(target).getChunks());
+                    } else {
+                        ProcessConfigMessagesUtility.processMessage("no-permission", commandSender);
+                    }
+
+                } else {
+                    if (commandSender.hasPermission("pvdc.get")) {
+                        ProcessConfigMessagesUtility.processMessage("view-distance-get-msg", PlayerUtility.getPlayerDataHandler(target).getChunks(), target, commandSender);
+                    } else {
+                        ProcessConfigMessagesUtility.processMessage("no-permission", commandSender);
+                    }
+                }
+            }
+        }
+    }
+}
