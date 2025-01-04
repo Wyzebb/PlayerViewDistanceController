11d303b02527b9fe6a8637d37250502ae336a237
Wyzebb <BenSJSmith8@gmail.com>
2025-01-04 13:28:58 +0000

    Add AFK check system (unfinished!)

--- src/main/java/me/wyzebb/playerviewdistancecontroller/PlayerViewDistanceController.java
+++ src/main/java/me/wyzebb/playerviewdistancecontroller/PlayerViewDistanceController.java
@@ -2,15 +2,22 @@ package me.wyzebb.playerviewdistancecontroller;
 
 import me.wyzebb.playerviewdistancecontroller.commands.CommandManager;
 import me.wyzebb.playerviewdistancecontroller.events.JoinLeaveEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
+import java.util.UUID;
 
 public final class PlayerViewDistanceController extends JavaPlugin {
     private FileConfiguration prefixesConfig;
+    private Map<UUID, Integer> playerAfkMap = new HashMap<>();
 
     @Override
     public void onEnable() {
@@ -27,6 +34,32 @@ public final class PlayerViewDistanceController extends JavaPlugin {
         // Register commands and tab completer
         Objects.requireNonNull(getCommand("viewdistance")).setExecutor(new CommandManager(this));
         Objects.requireNonNull(getCommand("viewdistance")).setTabCompleter(new CommandManager(this));
+
+        // Start AFK checker if enabled in the config
+        if (getConfig().getBoolean("afk-chunk-limiter")) new CheckAfk().runTaskTimer(this, 0L, 20L);
+    }
+
+    public void updateLastMoved(Player player) {
+        playerAfkMap.put(player.getUniqueId(), (int) System.currentTimeMillis());
+    }
+
+    private class CheckAfk extends BukkitRunnable {
+        @Override
+        public void run() {
+            getLogger().warning("CHECK");
+            int currentTime = (int) System.currentTimeMillis();
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                if (!player.isOp()) {
+                    UUID playerId = player.getUniqueId();
+                    int lastMoved = playerAfkMap.getOrDefault(playerId, currentTime);
+
+                    if (currentTime - lastMoved >= getConfig().getInt("afkTime")) {
+                        // SET VIEW DISTANCE AND DO NOT SAVE THAT TO FILE: SAVE ORIGINAL TO FILE HERE FIRST
+                        getLogger().warning("AFK");
+                    }
+                }
+            }
+        }
     }
 
     public FileConfiguration getPrefixesConfig() {
