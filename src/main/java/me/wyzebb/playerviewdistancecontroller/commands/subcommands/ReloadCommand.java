491262b3d741591aa002285d731d70d63246846b
Wyzebb <BenSJSmith8@gmail.com>
2025-01-04 11:07:09 +0000

    Make reload config actually work

--- src/main/java/me/wyzebb/playerviewdistancecontroller/commands/subcommands/ReloadCommand.java
+++ src/main/java/me/wyzebb/playerviewdistancecontroller/commands/subcommands/ReloadCommand.java
@@ -32,9 +32,9 @@ public class ReloadCommand extends SubCommand {
     @Override
     public void performCommand(CommandSender commandSender, String[] args) {
         if (commandSender.hasPermission("pvdc.reload")) {
-            plugin.saveDefaultConfig();
             plugin.getConfig().options().copyDefaults(true);
             createPrefixesConfig();
+            plugin.reloadConfig();
             ProcessConfigMessagesUtility.processMessage("reload-config-msg", commandSender);
         } else {
             ProcessConfigMessagesUtility.processMessage("no-permission", commandSender);
