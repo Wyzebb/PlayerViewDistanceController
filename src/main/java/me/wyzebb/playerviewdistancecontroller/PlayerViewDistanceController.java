e265d6812b17345993274ae23286b4a9a1c94061
Wyzebb <BenSJSmith8@gmail.com>
2025-01-06 21:10:21 +0000

    Support Folia scheduler

--- src/main/java/me/wyzebb/playerviewdistancecontroller/PlayerViewDistanceController.java
+++ src/main/java/me/wyzebb/playerviewdistancecontroller/PlayerViewDistanceController.java
@@ -1,5 +1,6 @@
 package me.wyzebb.playerviewdistancecontroller;
 
+import com.tcoded.folialib.FoliaLib;
 import me.wyzebb.playerviewdistancecontroller.commands.CommandManager;
 import me.wyzebb.playerviewdistancecontroller.data.PlayerDataHandler;
 import me.wyzebb.playerviewdistancecontroller.events.JoinLeaveEvent;
@@ -11,7 +12,6 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
 
 import java.io.File;
 import java.util.HashMap;
@@ -22,6 +22,8 @@ import java.util.UUID;
 public final class PlayerViewDistanceController extends JavaPlugin {
     private FileConfiguration prefixesConfig;
     private Map<UUID, Integer> playerAfkMap = new HashMap<>();
+
+    FoliaLib foliaLib = new FoliaLib(this);
 
     @Override
     public void onEnable() {
@@ -42,9 +44,10 @@ public final class PlayerViewDistanceController extends JavaPlugin {
 
         // Start AFK checker if enabled in the config
         if (getConfig().getBoolean("afk-chunk-limiter")) {
-            new CheckAfk().runTaskTimer(this, 0, 20L);
+            scheduleAfkChecker();
         }
     }
+
 
     public void updateLastMoved(Player player) {
         final UUID playerId = player.getUniqueId();
@@ -62,21 +65,22 @@ public final class PlayerViewDistanceController extends JavaPlugin {
         playerAfkMap.put(playerId, (int) System.currentTimeMillis());
     }
 
-    private class CheckAfk extends BukkitRunnable {
-        @Override
-        public void run() {
-            int currentTime = (int) System.currentTimeMillis();
-            for (Player player : Bukkit.getOnlinePlayers()) {
-                final UUID playerId = player.getUniqueId();
-                int lastMoved = playerAfkMap.getOrDefault(playerId, currentTime);
+    private void scheduleAfkChecker() {
+        foliaLib.getScheduler().runTimer(this::checkAfk, 0, 20);
+    }
 
-                if ((currentTime - lastMoved > (getConfig().getInt("afkTime")) * 1000) && playerAfkMap.get(playerId) != 0)  {
-                    player.setViewDistance(2);
-                    playerAfkMap.put(playerId, 0);
+    private void checkAfk() {
+        int currentTime = (int) System.currentTimeMillis();
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            final UUID playerId = player.getUniqueId();
+            int lastMoved = playerAfkMap.getOrDefault(playerId, currentTime);
 
-                    ProcessConfigMessagesUtility.processMessage("afk-msg", player);
-                    ProcessConfigMessagesUtility.processMessage("afk-msg-console", player, getServer().getConsoleSender());
-                }
+            if ((currentTime - lastMoved > (getConfig().getInt("afkTime")) * 1000) && playerAfkMap.get(playerId) != 0) {
+                player.setViewDistance(2);
+                playerAfkMap.put(playerId, 0);
+
+                ProcessConfigMessagesUtility.processMessage("afk-msg", player);
+                ProcessConfigMessagesUtility.processMessage("afk-msg-console", player, getServer().getConsoleSender());
             }
         }
     }
