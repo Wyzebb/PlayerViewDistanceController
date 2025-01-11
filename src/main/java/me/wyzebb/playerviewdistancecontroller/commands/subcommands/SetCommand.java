86b825b9618f8e3ebfc3263fc8c804413a068572
Wyzebb <BenSJSmith8@gmail.com>
2025-01-11 20:33:34 +0000

    Fix not able to set chunks equal to max

--- src/main/java/me/wyzebb/playerviewdistancecontroller/commands/subcommands/SetCommand.java
+++ src/main/java/me/wyzebb/playerviewdistancecontroller/commands/subcommands/SetCommand.java
@@ -4,19 +4,11 @@ import me.wyzebb.playerviewdistancecontroller.data.LuckPermsDataHandler;
 import me.wyzebb.playerviewdistancecontroller.utility.ClampAmountUtility;
 import me.wyzebb.playerviewdistancecontroller.utility.DataProcessorUtility;
 import me.wyzebb.playerviewdistancecontroller.utility.ProcessConfigMessagesUtility;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class SetCommand extends SubCommand {
-
-    private final MiniMessage mm;
-
-    public SetCommand() {
-        this.mm = MiniMessage.miniMessage();
-    }
 
     @Override
     public String getName() {
@@ -80,14 +72,11 @@ public class SetCommand extends SubCommand {
     private void setSelf(CommandSender commandSender, int amount) {
         if (commandSender.hasPermission("pvdc.set-self")) {
             int luckpermsMax = LuckPermsDataHandler.getLuckpermsDistance((Player) commandSender);
-            if (luckpermsMax > amount || commandSender.hasPermission("pvdc.bypass-maxdistance")) {
+            if (luckpermsMax >= amount || commandSender.hasPermission("pvdc.bypass-maxdistance")) {
                 ProcessConfigMessagesUtility.processMessage("self-view-distance-change-msg", commandSender, amount);
                 DataProcessorUtility.processData((Player) commandSender, amount);
             } else {
-                Component parsed = mm.deserialize("<yellow><b>(!)</b> <u><click:open_url:'https://modrinth.com/plugin/pvdc'><hover:show_text:'<green>Click to go to the plugin page</green>'>New update available for PVDC</hover></click></u>! Update from <red><b>v1.6.0</b></red> to <green><b>v2.0.0</b></green> to get the best experience!</yellow>");
-
-                commandSender.sendMessage(parsed);
-//                ProcessConfigMessagesUtility.processMessage("chunks-too-high", commandSender, luckpermsMax);
+                ProcessConfigMessagesUtility.processMessage("chunks-too-high", commandSender, luckpermsMax);
             }
 
         } else {
