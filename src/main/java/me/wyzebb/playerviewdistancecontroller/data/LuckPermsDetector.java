ee005fc0da964a732e018f97c4ee0515913b912f
Wyzebb <BenSJSmith8@gmail.com>
2025-01-18 13:45:03 +0000

    Add a method to detect LuckPerms without feedback messages

--- src/main/java/me/wyzebb/playerviewdistancecontroller/data/LuckPermsDetector.java
+++ src/main/java/me/wyzebb/playerviewdistancecontroller/data/LuckPermsDetector.java
@@ -3,7 +3,7 @@ package me.wyzebb.playerviewdistancecontroller.data;
 import static me.wyzebb.playerviewdistancecontroller.PlayerViewDistanceController.plugin;
 
 public class LuckPermsDetector {
-    public static boolean detectLuckPerms() {
+    public static boolean detectLuckPermsWithMsg() {
         try {
             Class.forName("net.luckperms.api.LuckPerms");
             plugin.getLogger().info("Enabling LuckPerms Hook");
@@ -11,6 +11,16 @@ public class LuckPermsDetector {
 
         } catch (Exception ex) {
             plugin.getLogger().warning("LuckPerms is not running on this server: it is optional, but it extends the plugin's functionality!");
+            return false;
+        }
+    }
+
+    public static boolean detectLuckPerms() {
+        try {
+            Class.forName("net.luckperms.api.LuckPerms");
+            return true;
+
+        } catch (Exception ex) {
             return false;
         }
     }
