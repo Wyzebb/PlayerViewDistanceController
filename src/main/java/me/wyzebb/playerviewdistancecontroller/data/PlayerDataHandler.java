7a7e791ba3114b7cb83cf35a5f963ff4e4543fbf
Wyzebb <BenSJSmith8@gmail.com>
2025-04-09 18:02:32 +0100

    Add ping mode and chunks to data handler

--- src/main/java/me/wyzebb/playerviewdistancecontroller/data/PlayerDataHandler.java
+++ src/main/java/me/wyzebb/playerviewdistancecontroller/data/PlayerDataHandler.java
@@ -3,6 +3,8 @@ package me.wyzebb.playerviewdistancecontroller.data;
 public class PlayerDataHandler {
     private int chunks;
     private int chunksOthers;
+    private boolean pingMode;
+    private int chunksPing;
 
     public int getChunks() {
         return chunks;
@@ -12,11 +14,27 @@ public class PlayerDataHandler {
         return chunksOthers;
     }
 
+    public boolean isPingMode() {
+        return pingMode;
+    }
+
+    public int getChunksPing() {
+        return chunksPing;
+    }
+
     public void setChunks(int chunks) {
         this.chunks = chunks;
     }
 
     public void setChunksOthers(int chunksOthers) {
         this.chunksOthers = chunksOthers;
+    }
+
+    public void setPingMode(boolean pingMode) {
+        this.pingMode = pingMode;
+    }
+
+    public void setChunksPing(int chunksPing) {
+        this.chunksPing = chunksPing;
     }
 }
