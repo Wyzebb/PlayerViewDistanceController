6b32b738181369a0b668124cbd64bca99ea004d0
Wyzebb <BenSJSmith8@gmail.com>
2025-06-24 19:16:16 +0100

    Remove reload command as it the reloaded values aren't used (will re-add this properly in the future)

--- src/main/java/me/wyzebb/playerviewdistancecontroller/commands/CommandManager.java
+++ src/main/java/me/wyzebb/playerviewdistancecontroller/commands/CommandManager.java
@@ -26,7 +26,6 @@ public class CommandManager implements TabExecutor {
         subcommands.add(new SetOnlineCommand());
         subcommands.add(new GetCommand());
         subcommands.add(new ResetCommand());
-        subcommands.add(new ReloadCommand());
         subcommands.add(new HelpCommand());
         subcommands.add(new PingCommand());
         subcommands.add(new DynamicModeCommand());
