2d491925d3e4ba23822e3c88197bb35856e0b0fe
Wyzebb <BenSJSmith8@gmail.com>
2025-06-29 17:21:41 +0100

    Take world context into account when using LuckPerms

--- src/main/java/me/wyzebb/playerviewdistancecontroller/data/LuckPermsDataHandler.java
+++ src/main/java/me/wyzebb/playerviewdistancecontroller/data/LuckPermsDataHandler.java
@@ -1,6 +1,7 @@
 package me.wyzebb.playerviewdistancecontroller.data;
 
 import net.luckperms.api.LuckPerms;
+import net.luckperms.api.context.ContextSet;
 import net.luckperms.api.model.user.User;
 import net.luckperms.api.query.QueryOptions;
 import org.bukkit.Bukkit;
@@ -10,6 +11,8 @@ import org.bukkit.plugin.RegisteredServiceProvider;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import static me.wyzebb.playerviewdistancecontroller.PlayerViewDistanceController.plugin;
 
 public class LuckPermsDataHandler {
 
@@ -26,7 +29,7 @@ public class LuckPermsDataHandler {
             // Regular expression to match permissions like pvdc.maxdistance.7
             Pattern pattern = Pattern.compile("pvdc\\.maxdistance\\.(\\d+)");
 
-            int max = getMaxDistance(user, pattern);
+            int max = getMaxDistance(user, pattern, player);
 
             if (max == 0) {
                 return 32;
@@ -35,22 +38,51 @@ public class LuckPermsDataHandler {
             }
     }
 
-    private static int getMaxDistance(User user, Pattern pattern) {
+    private static int getMaxDistance(User user, Pattern pattern, OfflinePlayer player) {
         int maxDistance = 0;
+        int maxDistanceContext = 0;
 
         for (var node : user.resolveInheritedNodes(QueryOptions.nonContextual())) {
             String permission = node.getKey();
             Matcher matcher = pattern.matcher(permission);
 
             if (matcher.matches()) {
-                // Extract the number from the permission
-                int distance = Integer.parseInt(matcher.group(1));
-                if (distance > maxDistance) {
-                    maxDistance = distance;
+                ContextSet contextSet = node.getContexts();
+
+                if (player.isOnline()) {
+                    if (contextSet.containsKey("world")) {
+                        String playerWorldName = player.getPlayer().getWorld().getName();
+                        plugin.getLogger().warning("plrworld: " + playerWorldName);
+                        String worldName = contextSet.getAnyValue("world").orElse("unknown");
+                        plugin.getLogger().warning("world: " + worldName);
+
+                        // Skip the node if it's for a different world
+                        if (playerWorldName.equals(worldName)) {
+                            plugin.getLogger().warning("same1");
+                            // Extract the number from the permission
+                            int distance = Integer.parseInt(matcher.group(1));
+                            if (distance > maxDistanceContext) {
+                                maxDistanceContext = distance;
+                                plugin.getLogger().warning("max context: " + maxDistanceContext);
+                            }
+                            continue;
+                        } else {
+                            plugin.getLogger().warning("skip");
+                        }
+                    } else {
+                        plugin.getLogger().warning("part2");
+
+                        // Extract the number from the permission
+                        int distance = Integer.parseInt(matcher.group(1));
+                        if (distance > maxDistance) {
+                            maxDistance = distance;
+                            plugin.getLogger().warning("max2: " + maxDistance);
+                        }
+                    }
                 }
             }
         }
 
-        return maxDistance;
+        return Math.max(maxDistance, maxDistanceContext);
     }
 }
