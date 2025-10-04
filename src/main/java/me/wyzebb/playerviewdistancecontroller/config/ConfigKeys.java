6706fc38e2a76c4c243309f9f949f55d3dc41e8e
Wyzebb <wyzebbmc@gmail.com>
2025-10-04 10:52:49 +0100

    feat: change config keys to be more consistent

--- src/main/java/me/wyzebb/playerviewdistancecontroller/config/ConfigKeys.java
+++ src/main/java/me/wyzebb/playerviewdistancecontroller/config/ConfigKeys.java
@@ -4,7 +4,11 @@ package me.wyzebb.playerviewdistancecontroller.config;
  * Configuration key constants
  */
 public final class ConfigKeys {
-    //TODO: make config keys more consistent and use these values throughout codebase
+    // Colour and Prefix Settings
+    public static final String PREFIX = "prefix";
+    public static final String GENERIC_COLOR = "color";
+    public static final String SUCCESS_COLOR = "success-color";
+    public static final String ERROR_COLOR = "error-color";
 
     // View Distance Settings
     public static final String DEFAULT_DISTANCE = "default-distance";
@@ -14,29 +18,24 @@ public final class ConfigKeys {
 
     // AFK Settings
     public static final String AFK_CHUNK_LIMITER = "afk-chunk-limiter";
-    public static final String AFK_ON_JOIN = "afkOnJoin";
-    public static final String AFK_TIME = "afkTime";
-    public static final String AFK_CHUNKS = "afkChunks";
-    public static final String ZERO_CHUNKS_AFK = "zero-chunks-afk";
-    public static final String SPECTATORS_CAN_AFK = "spectators-can-afk";
+    public static final String AFK_ON_JOIN = "afk-on-join";
+    public static final String AFK_TIME = "afk-time";
+    public static final String AFK_CHUNKS = "afk-chunks";
+    public static final String AFK_SPECTATORS = "afk-spectators";
+    public static final String AFK_VOID_WORLD = "afk-void-world";
 
     // Message Settings
     public static final String LANGUAGE = "language";
-    public static final String DISPLAY_MSG_ON_JOIN = "display-msg-on-join";
-    public static final String DISPLAY_MAX_JOIN_MSG = "display-max-join-msg";
-    public static final String DISPLAY_MAX_CHANGE_JOIN_MSG = "display-max-change-join-msg";
-    public static final String SEND_MSG_ON_WORLD_CHANGE = "send-msg-on-world-change";
-
-    // Colour Settings
-    public static final String COLOUR = "colour";
-    public static final String ERROR_COLOUR = "error-colour";
-    public static final String SUCCESS_COLOUR = "success-colour";
+    public static final String MSG_ON_JOIN = "msg-on-join";
+    public static final String MSG_ON_JOIN_MAX = "msg-on-join-max";
+    public static final String MSG_ON_JOIN_MAX_VIEW = "msg-on-join-max-view";
+    public static final String MSG_ON_WORLD_CHANGE = "msg-on-world-change";
 
     // Feature Settings
     public static final String USE_CLIENT_VIEW_DISTANCE = "use-client-view-distance";
     public static final String SYNC_SIMULATION_DISTANCE = "sync-simulation-distance";
     public static final String UPDATE_CHECKER_ENABLED = "update-checker-enabled";
-    public static final String RECALCULATE_VD_ON_WORLD_CHANGE = "recalculate-vd-on-world-change";
+    public static final String RECALCULATE_VD_ON_WORLD_CHANGE = "world-change-recalculate-view";
 
     // Private constructor to prevent instantiation
     private ConfigKeys() {
