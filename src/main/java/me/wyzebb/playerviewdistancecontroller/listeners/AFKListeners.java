39360ad0df92b3b4ab41612c33c2401bdbffa1f1
Noctiro <49872497+noctiro@users.noreply.github.com>
2026-02-08 01:49:35 +0800

    fix: prevent AFK false positives when riding vehicles
    Update player's last moved time when they are riding vehicles (minecarts, boats, etc.) to avoid being incorrectly marked as AFK.

--- src/main/java/me/wyzebb/playerviewdistancecontroller/listeners/AFKListeners.java
+++ src/main/java/me/wyzebb/playerviewdistancecontroller/listeners/AFKListeners.java
@@ -1,10 +1,13 @@
 package me.wyzebb.playerviewdistancecontroller.listeners;
 
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Vehicle;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.vehicle.VehicleMoveEvent;
 
 import static me.wyzebb.playerviewdistancecontroller.PlayerViewDistanceController.plugin;
 
@@ -13,6 +16,16 @@ public class AFKListeners implements Listener {
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event) {
         plugin.updateLastMoved(event.getPlayer());
+    }
+
+    @EventHandler
+    public void onVehicleMove(VehicleMoveEvent event) {
+        Vehicle vehicle = event.getVehicle();
+        vehicle.getPassengers().forEach(passenger -> {
+           if (passenger instanceof Player player) {
+               plugin.updateLastMoved(player);
+           }
+        });
     }
 
     @EventHandler
