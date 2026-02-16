e1b70b4670a8f0b758dc63950cb94e5c10b0eff5
Wyzebb <wyzebbmc@gmail.com>
2026-02-16 13:32:35 +0000

    feat: add more db and yaml decision get/set methods for vd data, add world checks for getting chunks

--- src/main/java/me/wyzebb/playerviewdistancecontroller/data/Storage.java
+++ src/main/java/me/wyzebb/playerviewdistancecontroller/data/Storage.java
@@ -11,17 +11,22 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import static me.wyzebb.playerviewdistancecontroller.PlayerViewDistanceController.plugin;
 
 public class Storage {
-    static final boolean sqlDb = plugin.getPluginConfig().getDatabaseType() == Database.StorageType.MYSQL
+    private static final boolean sqlDb = plugin.getPluginConfig().getDatabaseType() == Database.StorageType.MYSQL
             || plugin.getPluginConfig().getDatabaseType() == Database.StorageType.SQLITE;
 
-    public static void setChunks(OfflinePlayer player, String world, int chunks) {
+    public static boolean isSqlDb() {
+        return sqlDb;
+    }
+
+    public static void setChunks(OfflinePlayer player, UUID world, int chunks) {
         if (sqlDb) {
             Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
-                Database.getInstance().updateVd(player, world, chunks);
+                Database.getInstance().updateInt("vd", player, world.toString(), chunks);
             });
         } else {
             PlayerDataHandler handler = DataHandlerHandler.getPlayerDataHandler(player);
@@ -29,18 +34,44 @@ public class Storage {
         }
     }
 
-    public static int getChunks(OfflinePlayer player, String world) {
+    public static void setAdminChunks(OfflinePlayer player, UUID world, int chunks) {
+        if (sqlDb) {
+            Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
+                Database.getInstance().updateInt("vd_admin", player, world.toString(), chunks);
+            });
+        } else {
+            PlayerDataHandler handler = DataHandlerHandler.getPlayerDataHandler(player);
+            handler.setAdminChunks(chunks);
+        }
+    }
+
+    public static void setPingMode(OfflinePlayer player, boolean pingMode) {
+        if (sqlDb) {
+            Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
+                Database.getInstance().updateBoolean("ping_mode", player, pingMode);
+            });
+        } else {
+            PlayerDataHandler handler = DataHandlerHandler.getPlayerDataHandler(player);
+            handler.setPingMode(pingMode);
+        }
+    }
+
+    public static int getChunks(OfflinePlayer player, UUID world) {
         if (sqlDb) {
             final int[] value = {0};
             Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
                 try {
-                    final String query = "SELECT vd FROM vdData WHERE player_uuid = ?";
+                    String query = "SELECT vd FROM vdData WHERE player_uuid = ?";
+
+                    if (plugin.getPluginConfig().isWorldIndependent()) query += " AND world = ? ";
 
                     final PreparedStatement statement = Database.getConnection().prepareStatement(query);
                     statement.setString(1, player.getName());
+                    if (plugin.getPluginConfig().isWorldIndependent()) statement.setString(2, world.toString());
 
                     final ResultSet resultSet = statement.executeQuery();
                     value[0] = resultSet.getInt(0);
+                    //TODO Issue when multiple rows and world independence off
 
                     resultSet.close();
                     statement.close();
@@ -53,6 +84,64 @@ public class Storage {
         } else {
             PlayerDataHandler handler = DataHandlerHandler.getPlayerDataHandler(player);
             return handler.getChunks();
+        }
+    }
+
+    public static int getAdminChunks(OfflinePlayer player, UUID world) {
+        if (sqlDb) {
+            final int[] value = {0};
+            Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
+                try {
+                    String query = "SELECT vd_admin FROM vdData WHERE player_uuid = ?";
+
+                    if (plugin.getPluginConfig().isWorldIndependent()) query += " AND world = ? ";
+
+                    final PreparedStatement statement = Database.getConnection().prepareStatement(query);
+                    statement.setString(1, player.getName());
+                    if (plugin.getPluginConfig().isWorldIndependent()) statement.setString(2, world.toString());
+
+                    final ResultSet resultSet = statement.executeQuery();
+                    value[0] = resultSet.getInt(0);
+                    //TODO Issue when multiple rows and world independence off
+
+                    resultSet.close();
+                    statement.close();
+                } catch (final SQLException e) {
+                    e.printStackTrace();
+                }
+            });
+
+            return value[0];
+        } else {
+            PlayerDataHandler handler = DataHandlerHandler.getPlayerDataHandler(player);
+            return handler.getAdminChunks();
+        }
+    }
+
+    public static boolean isPingMode(OfflinePlayer player) {
+        if (sqlDb) {
+            final boolean[] value = {false};
+            Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
+                try {
+                    String query = "SELECT ping_mode FROM users WHERE player_uuid = ?";
+
+                    final PreparedStatement statement = Database.getConnection().prepareStatement(query);
+                    statement.setString(1, player.getName());
+
+                    final ResultSet resultSet = statement.executeQuery();
+                    value[0] = resultSet.getBoolean(0);
+
+                    resultSet.close();
+                    statement.close();
+                } catch (final SQLException e) {
+                    e.printStackTrace();
+                }
+            });
+
+            return value[0];
+        } else {
+            PlayerDataHandler handler = DataHandlerHandler.getPlayerDataHandler(player);
+            return handler.isPingMode();
         }
     }
 
